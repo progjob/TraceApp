c0c3c70c1ad833ad749a5be5ba426c0ffdf5f749
Dmitry Evstratov <progjob@mail.ru>
2021-04-22 17:19:19 +0300

    Initial Commit

---
+++ FridaTraceApp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  FridaTraceApp
+//
+//  Created by Dmitry on 22.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ FridaTraceApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  FridaTraceApp
+//
+//  Created by Dmitry on 22.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
