6394e0f9bff47cdccf12f51e188f40e4e84ffd72
Dmitry Evstratov <progjob@mail.ru>
2021-04-23 00:06:56 +0300

    Use bunch of classes to read/write data in local storage

--- FridaTraceApp/ViewController.m
+++ FridaTraceApp/ViewController.m
@@ -15,8 +15,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
+- (IBAction)uiApplication {
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"/open/Url"]];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"/open/Url/options/completionHandler"] options:@{} completionHandler:nil];
+    [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"/can/Open/Url"]];
+}
+
+- (IBAction)fileManager {
+    [[NSFileManager defaultManager] fileExistsAtPath:@"/file/Exists/At/Path"];
+    [[NSFileManager defaultManager] isReadableFileAtPath:@"/is/Readble/At/Path"];
+    [[NSFileManager defaultManager] createFileAtPath:@"/create/file/at/urk" contents:nil attributes:nil];
+}
+
+
+- (IBAction)userDefaults {
+    [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"key1"];
+    [[NSUserDefaults standardUserDefaults] stringForKey:@"key2"];
+}
+
+- (IBAction)string {
+    [[NSString alloc] initWithContentsOfFile:@"file.txt" encoding:nil error:nil];
+    
+    NSError *error;
+    NSString *stringToBeWritten = @"This is a test.";
+    [stringToBeWritten writeToFile:@"/private/jailbreak.txt"
+                        atomically:YES
+                          encoding:NSUTF8StringEncoding
+                             error:&error];
+
+}
+
+- (IBAction)data {
+    [[NSData new] writeToFile:@"/data/write/to/fiel" atomically:YES];
+    [NSData dataWithContentsOfURL:[NSURL URLWithString:@"/urk/with/string"]];
+}
+
+- (IBAction)array {
+    [NSArray arrayWithContentsOfFile:@"/array/with/Content/ofFiel"];
+    [@[] writeToFile:@"/write/to/file" atomically:YES];
+}
+
+- (IBAction)dictionary {
+    NSDictionary *dic = @{};
+    [dic writeToFile:@"/write/to/file" atomically:YES];
+    
+    [NSDictionary dictionaryWithContentsOfFile:@"/dict/from/file"];
+}
 
 @end
